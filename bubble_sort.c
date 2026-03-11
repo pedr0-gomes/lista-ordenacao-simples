bcc72228b87308c1157af32f4c5d96ef81a3023b
pedr0-gomes <gomes.pedro@aluno.ufca.edu.br>
2026-03-11 14:29:05 -0300

    feat(bubble-sort): add estrutura inicial com header

---
+++ bubble_sort.c
@@ -0,0 +1,51 @@
+/*
+Enunciado:
+Implemente void bubbleSort(int v[], int n) para ordenar em ordem crescente.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "sorting.h"
+
+/*
+Função principal do exercício
+*/
+
+
+int main(void)
+{
+    int v1[] = {1,2,3,4,5,6,7,8,9,10};
+    int v2[] = {10,9,8,7,6,5,4,3,2,1};
+    int v3[] = {5,8,5,1,3,7,5,4,2,9};
+    int v4[] = {3,1,4,5,2,9,8,7,10,6};
+    int n = 10;
+
+    printf("Teste 1:\n");
+    printf("Vetor Antes da Ordenação:\n");
+    imprimir_array(v1,n);
+    // chamada da função de ordenação
+    printf("Vetor Depois da Ordenação:\n");
+    imprimir_array(v1,n);
+
+    printf("Teste 2:\n");
+    printf("Vetor Antes da Ordenação:\n");
+    imprimir_array(v2,n);
+    // chamada da função de ordenação
+    printf("Vetor Depois da Ordenação:\n");
+    imprimir_array(v2,n);
+
+    printf("Teste 3:\n");
+    printf("Vetor Antes da Ordenação:\n");
+    imprimir_array(v3,n);
+    // chamada da função de ordenação
+    printf("Vetor Depois da Ordenação:\n");
+    imprimir_array(v3,n);
+
+    printf("Teste 4:\n");
+    printf("Vetor Antes da Ordenação:\n");
+    imprimir_array(v4,n);
+    // chamada da função de ordenação
+    printf("Vetor Depois da Ordenação:\n");
+    imprimir_array(v4,n);
+
+    return 0;
+}
