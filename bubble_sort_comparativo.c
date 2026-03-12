6d9015c51dfa3e8358dd34c216944dd43d5033f4
pedr0-gomes <gomes.pedro@aluno.ufca.edu.br>
2026-03-12 15:16:54 -0300

    feat(bubble_sort_comparativo): comparar o numero de trocas e comparacoes entre o bubble normal e o optimizado

--- bubble_sort_comparativo.c
+++ bubble_sort_comparativo.c
@@ -12,42 +12,94 @@ Qual caso reduz mais operações com early stop?
 /*
 Função principal do exercício
 */
+void bubbleSortInstrumentado(int v[],int n)
+{
+    int comparacoes = 0,trocas = 0;
+    for (int i = 0; i < n-1;i++)
+    {
+        for (int j = 0;j < n - i - 1;j++)
+        {
+            comparacoes++;
+            if (v[j] > v[j+1])
+            {
+                trocas++;
+                swap(&v[j],&v[j+1]);
+            }
+        }
+    }
+    printf("Numero de comparacoes = %d\n",comparacoes);
+    printf("Numero de trocas = %d\n",trocas);
+}
+
+void bubbleSortOptimizado(int v[],int n)
+{
+    int comparacoes = 0, trocas = 0;
+    for (int i = 0; i < n-1;i++)
+    {
+        int houve_troca = 0;
+        comparacoes++;
+        for (int j = 0;j < n - i - 1;j++)
+        {
+            if (v[j] > v[j+1])
+            {
+                swap(&v[j],&v[j+1]);
+                houve_troca = 1;
+                trocas++;
+            }
+        }
+        if (houve_troca == 0) 
+        {
+            break;
+        }
+    }
+    printf("Numero de comparacoes = %d\n",comparacoes);
+    printf("Numero de trocas = %d\n",trocas); 
+}
 
 int main(void)
 {
-    int v1[] = {1,2,3,4,5,6,7,8,9,10};
-    int v2[] = {10,9,8,7,6,5,4,3,2,1};
-    int v3[] = {5,8,5,1,3,7,5,4,2,9};
-    int v4[] = {3,1,4,5,2,9,8,7,10,6};
-    int n = 10;
+    // quero testar com n = 20
 
-    printf("Teste 1:\n");
+    int v1[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int v2[] = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+    int v3[] = {5,8,5,1,3,7,5,4,2,9,6,10,7,3,8,10,6,9,2,4};
+    int n = 20;
+
+    printf("Caso 1: Vetor Ordenado\n");
     printf("Vetor Antes da Ordenação:\n");
     imprimir_array(v1,n);
-    // chamada da função de ordenação
+    printf("Bubble Sort Instrumentado:\n");
+    bubbleSortInstrumentado(v1,n);
+    // resetando o vetor para o teste do bubble sort otimizado
+    int v1_reset[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    printf("Bubble Sort Otimizado:\n");
+    bubbleSortOptimizado(v1_reset,n);
     printf("Vetor Depois da Ordenação:\n");
-    imprimir_array(v1,n);
+    imprimir_array(v1_reset,n);
 
-    printf("Teste 2:\n");
+    printf("Caso 2: Vetor Reverso\n");
     printf("Vetor Antes da Ordenação:\n");
     imprimir_array(v2,n);
-    // chamada da função de ordenação
+    printf("Bubble Sort Instrumentado:\n");
+    bubbleSortInstrumentado(v2,n);
+    // resetando o vetor para o teste do bubble sort otimizado
+    int v2_reset[] = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+    printf("Bubble Sort Otimizado:\n");
+    bubbleSortOptimizado(v2_reset,n);
     printf("Vetor Depois da Ordenação:\n");
-    imprimir_array(v2,n);
+    imprimir_array(v2_reset,n);
 
-    printf("Teste 3:\n");
+    printf("Caso 3: Vetor Aleatório\n");
     printf("Vetor Antes da Ordenação:\n");
     imprimir_array(v3,n);
-    // chamada da função de ordenação
+    printf("Bubble Sort Instrumentado:\n");
+    bubbleSortInstrumentado(v3,n);
+    // resetando o vetor para o teste do bubble sort otimizado
+    int v3_reset[] = {5,8,5,1,3,7,5,4,2,9,6,10,7,3,8,10,6,9,2,4};
+    printf("Bubble Sort Otimizado:\n");
+    bubbleSortOptimizado(v3_reset,n);
     printf("Vetor Depois da Ordenação:\n");
-    imprimir_array(v3,n);
-
-    printf("Teste 4:\n");
-    printf("Vetor Antes da Ordenação:\n");
-    imprimir_array(v4,n);
-    // chamada da função de ordenação
-    printf("Vetor Depois da Ordenação:\n");
-    imprimir_array(v4,n);
+    imprimir_array(v3_reset,n);
 
     return 0;
 }
