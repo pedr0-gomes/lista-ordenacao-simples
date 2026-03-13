a2c4a2c6cea25b7ff2e7fc230f9ebc0fb6189293
pedr0-gomes <gomes.pedro@aluno.ufca.edu.br>
2026-03-13 13:26:37 -0300

    feat(insertion_sort_tracado): implementar insertionSort que exibe cada iteracao

--- insertion_sort_tracado.c
+++ insertion_sort_tracado.c
@@ -9,43 +9,37 @@ Para o vetor [5, 3, 4, 1, 2], escreva o vetor ao final de cada iteração do for
 /*
 Função principal do exercício
 */
+void insertionSortTracado(int *V,int n)
+{
+    for (int i = 1;i < n;i++)
+    {
+        printf("Para i = %d\n",i);
+        printf("Antes: ");
+        imprimir_array(V,n);
+        int chave = V[i];
+        int j = i;
+        while (j > 0 && chave < V[j-1])
+        {
+            V[j] = V[j-1];
+            j--;
+        }
+        V[j] = chave;
+        printf("Depois: ");
+        imprimir_array(V,n);
+        printf("\n");
+    }
+}
 
 
 int main(void)
 {
-    int v1[] = {1,2,3,4,5,6,7,8,9,10};
-    int v2[] = {10,9,8,7,6,5,4,3,2,1};
-    int v3[] = {5,8,5,1,3,7,5,4,2,9};
-    int v4[] = {3,1,4,5,2,9,8,7,10,6};
-    int n = 10;
-
-    printf("Teste 1:\n");
-    printf("Vetor Antes da Ordenação:\n");
-    imprimir_array(v1,n);
+    int v[] = {5, 3, 4, 1, 2};
+    int n = 5;
+    printf("Vetor antes da ordenação:\n");
+    imprimir_array(v,n);
+    printf("\n");
     // chamada da função de ordenação
-    printf("Vetor Depois da Ordenação:\n");
-    imprimir_array(v1,n);
-
-    printf("Teste 2:\n");
-    printf("Vetor Antes da Ordenação:\n");
-    imprimir_array(v2,n);
-    // chamada da função de ordenação
-    printf("Vetor Depois da Ordenação:\n");
-    imprimir_array(v2,n);
-
-    printf("Teste 3:\n");
-    printf("Vetor Antes da Ordenação:\n");
-    imprimir_array(v3,n);
-    // chamada da função de ordenação
-    printf("Vetor Depois da Ordenação:\n");
-    imprimir_array(v3,n);
-
-    printf("Teste 4:\n");
-    printf("Vetor Antes da Ordenação:\n");
-    imprimir_array(v4,n);
-    // chamada da função de ordenação
-    printf("Vetor Depois da Ordenação:\n");
-    imprimir_array(v4,n);
-
+    insertionSortTracado(v,n);
+    
     return 0;
 }
