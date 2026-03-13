f307c90f63645d032fba10e9ad5f794b23c2bad5
pedr0-gomes <gomes.pedro@aluno.ufca.edu.br>
2026-03-13 13:37:58 -0300

    feat(insertion_sort_instrumentado): add estrutura inicial com header

---
+++ insertion_sort_instrumentado.c
@@ -0,0 +1,53 @@
+/*
+Enunciado:
+Crie uma versão instrumentada que conte:
+- comparacoes: apenas avaliações de condição do tipo chave < V[j-1]
+- movimentacoes: atribuições no vetor V[...] - ...(inclui deslocamentos e inserção final da chave)
+Teste com vetores: ordenado, reverso, aleatório(n=20).
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "sorting.h"
+
+/*
+Função principal do exercício
+*/
+
+int main(void)
+{
+    int v1[] = {1,2,3,4,5,6,7,8,9,10};
+    int v2[] = {10,9,8,7,6,5,4,3,2,1};
+    int v3[] = {5,8,5,1,3,7,5,4,2,9};
+    int v4[] = {3,1,4,5,2,9,8,7,10,6};
+    int n = 10;
+
+    printf("Teste 1:\n");
+    printf("Vetor Antes da Ordenação:\n");
+    imprimir_array(v1,n);
+    // chamada da função de ordenação
+    printf("Vetor Depois da Ordenação:\n");
+    imprimir_array(v1,n);
+
+    printf("Teste 2:\n");
+    printf("Vetor Antes da Ordenação:\n");
+    imprimir_array(v2,n);
+    // chamada da função de ordenação
+    printf("Vetor Depois da Ordenação:\n");
+    imprimir_array(v2,n);
+
+    printf("Teste 3:\n");
+    printf("Vetor Antes da Ordenação:\n");
+    imprimir_array(v3,n);
+    // chamada da função de ordenação
+    printf("Vetor Depois da Ordenação:\n");
+    imprimir_array(v3,n);
+
+    printf("Teste 4:\n");
+    printf("Vetor Antes da Ordenação:\n");
+    imprimir_array(v4,n);
+    // chamada da função de ordenação
+    printf("Vetor Depois da Ordenação:\n");
+    imprimir_array(v4,n);
+
+    return 0;
+}
